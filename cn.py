2bc1d8038f64fa6c6ad415362c6e8811cb9b2641
Santiago Gonzalez <nojero@gmail.com>
2016-05-13 21:59:45 -0300

    Unificando archivos, separando en carpetas, agregando primer jugador

--- cn.py
+++ cn.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*
 import random
+import sys
 from wordlist import WORDS
 
 WORD_AMOUNT = 25
@@ -16,10 +17,13 @@ count_dict = {
 3:1,
 }
 
-def create_board(first=0):
+colors = {'azul':0,'rojo':1}
+
+def create_board(first='azul'):
     words = random.sample(WORDS,WORD_AMOUNT)
     board = {}
-    count_dict[first] += 1
+    board['first'] = first.upper()
+    count_dict[colors[first]] += 1
     count = 1
     for word in words:
         while 1:
@@ -33,18 +37,22 @@ def create_board(first=0):
     return board
 
 if __name__ == "__main__":
-    board = create_board()
-    template = open('template.html', 'r')
-    html = template.read()
-    template.close()
-    new_html = html.format(**board)
-    template_white = open('template_white.html', 'r')
-    html_white = template_white.read()
-    template_white.close()
-    new_html_white = html_white.format(**board)
-    f2 = open('board_color.html', 'w')
-    f2.write(new_html)
-    f2.close()
-    f2 = open('board_white.html', 'w')
-    f2.write(new_html_white)
-    f2.close()
+    args = sys.argv
+    if (len(args) < 2) or args[1] not in ('rojo','azul'):
+        print "Usage: python cn.py [rojo|azul]"
+    else:
+        board = create_board(first=args[1])
+        template = open('templates/template.html', 'r')
+        html = template.read()
+        template.close()
+        new_html = html.format(**board)
+        template_white = open('templates/template_white.html', 'r')
+        html_white = template_white.read()
+        template_white.close()
+        new_html_white = html_white.format(**board)
+        f2 = open('board_color.html', 'w')
+        f2.write(new_html)
+        f2.close()
+        f2 = open('board_white.html', 'w')
+        f2.write(new_html_white)
+        f2.close()
--- js/change.js
+++ js/change.js
@@ -2,12 +2,12 @@ $(function(){
     $("td").on('click',function(){
        var c = $(this).attr('class'); 
        if (c.indexOf("change") >= 0){
-         return true;
-       } else {
          if(confirm("Seguro que elegis la palabra: " + $(this).html() + '?')){
              $(this).removeClass(c);
-             $(this).addClass(c+'_change');
+             $(this).addClass(c.replace('_change',''));
          }
+       } else {
+         return true;
        }
     });
 });
